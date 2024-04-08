e8e1277334413b6ead63eb0922a57e118952ab60
YksRocks <yk66478@gmail.com>
2024-04-08 23:07:15 +0530

    Fix typo in domain name in OurTeam component

--- src/pages/OurTeam/OurTeam.jsx
+++ src/pages/OurTeam/OurTeam.jsx
@@ -80,7 +80,7 @@ function OurTeam() {
 
     {
       name: "Akshat Dwivedi",
-      domain: "Overall Coordinators",
+      domain: "Overall Coordinator",
       imgUrl: Akshat,
       linkedIn: "",
       gmail: "",
