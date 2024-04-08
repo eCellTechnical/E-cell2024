1589e709bfc9852f635b5f7f34b153ba8a7e00de
naksh1414 <nakshatramanglik14@gmail.com>
2024-04-08 17:50:05 +0530

    main

--- src/endeavour/Home/EndeavourHomePage.jsx
+++ src/endeavour/Home/EndeavourHomePage.jsx
@@ -69,11 +69,6 @@ function EndeavourHomePage() {
             </Suspense>
           </div>
 
-          <div className="w-full dark:bg-gray-900 bg-white pt-16 pb-10 z-10">
-            <Suspense fallback={<div>Loading...</div>}>
-              <Speakers />
-            </Suspense>
-          </div>
           <div className="dark:bg-black bg-white pt-16 z-10 w-full">
             <Suspense fallback={<div>Loading...</div>}>
               <AllEvents />
@@ -82,6 +77,11 @@ function EndeavourHomePage() {
           <div className="w-full z-10">
             <Suspense fallback={<div>Loading...</div>}>
               <Sponsors />
+            </Suspense>
+          </div>
+          <div className="w-full dark:bg-gray-900 bg-white pt-16 pb-10 z-10">
+            <Suspense fallback={<div>Loading...</div>}>
+              <Speakers />
             </Suspense>
           </div>
         </div>
