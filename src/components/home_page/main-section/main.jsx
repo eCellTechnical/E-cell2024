ae8cab80886b8d25f8de36fd4a211dfe8115cceb
YksRocks <yk66478@gmail.com>
2024-04-24 14:50:34 +0530

    Fix PopUp.jsx and main.jsx

--- src/components/home_page/main-section/main.jsx
+++ src/components/home_page/main-section/main.jsx
@@ -114,7 +114,7 @@ const Main = () => {
           {!isPayment && (
             <div className="md:mr-8">
               <p className="text-md line-through text-gray-600">₹299</p>
-              <p className="text-3xl font-semibold">₹249</p>
+              <p className="text-3xl font-semibold">₹250</p>
             </div>
           )}
 
@@ -125,7 +125,7 @@ const Main = () => {
               </p>
             </button>
           ) : (
-            <button className="ml-10" onClick={() => handlePayClick(249)}>
+            <button className="ml-10" onClick={() => handlePayClick(250)}>
               <p className="text-lg w-[180px] md:text-xl font-semibold border-2 border-black p-5">
                 Register Now
               </p>
