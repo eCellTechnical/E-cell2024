b2b4d262a1eda98035541c90fdd2928f12d98324
YksRocks <yk66478@gmail.com>
2024-05-11 00:49:36 +0530

    chore: Update npm dependency to include react-component-export-image

--- src/endeavour/Profile/EndeavourProfile.jsx
+++ src/endeavour/Profile/EndeavourProfile.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useState, useCallback } from "react";
+import { useEffect, useState, useCallback, useRef } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
 import { useForm } from "react-hook-form";
@@ -14,12 +14,32 @@ import editb from "../../assets/Profile/icons8-edit-64.png";
 import eventb from "../../assets/Profile/icons8-calendar-list-64.png";
 import eventw from "../../assets/Profile/icons8-calendar-list-64 (1).png";
 
-import paymentb from "../../assets/Profile/icons8-exchange-rupee-64 (1).png";
-import paymentw from "../../assets/Profile/icons8-exchange-rupee-64.png";
+// import paymentb from "../../assets/Profile/icons8-exchange-rupee-64 (1).png";
+// import paymentw from "../../assets/Profile/icons8-exchange-rupee-64.png";
 
+import certb from "../../assets/Profile/certdark.png";
+import certw from "../../assets/Profile/certlight.png";
+
+import certt from "../../assets/Certificate_participants (1).png";
+import { exportComponentAsPNG } from "react-component-export-image";
+
+import "./EndeavourProfile.css";
 // import { Link } from "react-router-dom";
 
 function EndeavourProfile() {
+  let certificateWrapper1 = useRef();
+  let certificateWrapper2 = useRef();
+  let certificateWrapper3 = useRef();
+  let certificateWrapper4 = useRef();
+  let certificateWrapper5 = useRef();
+  let certificateWrapper6 = useRef();
+  let certificateWrapper7 = useRef();
+
+  const [getName, setGetName] = useState(true);
+  const [fullName, setFullName] = useState("");
+  const [collegeName, setCollegeName] = useState("");
+  const [participatedEventNames, setParticipatedEventNames] = useState([]);
+
   const { register, handleSubmit } = useForm();
   const [disable, setDisable] = useState(true);
   const history = useNavigate();
@@ -363,7 +383,6 @@ function EndeavourProfile() {
   };
 
   const paymentClicked = async (e) => {
-    e.preventDefault();
     try {
       const response = await axios.get(
         `https://e-cell2024backend-production.up.railway.app/payment-info/${localStorage.getItem(
@@ -384,7 +403,67 @@ function EndeavourProfile() {
         theme: "colored",
       });
     }
-    setTab(2);
+  };
+
+  const certClicked = async () => {
+    // e.preventDefault();
+
+    try {
+      const response = await axios.get(
+        `https://e-cell2024backend-production.up.railway.app/cert-info/${localStorage.getItem(
+          "userId"
+        )}`
+      );
+
+      if (response.data.msg == "no") {
+        setGetName(false);
+      } else {
+        setFullName(response.data.realName);
+        setCollegeName(response.data.college);
+        setParticipatedEventNames(response.data.participatedEventNames);
+        setGetName(true);
+      }
+      setTab(2);
+    } catch (error) {
+      toast.error("Bad network error,Please try again after sometime", {
+        position: "top-center",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        theme: "colored",
+      });
+    }
+  };
+
+  const certCreate = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        `https://e-cell2024backend-production.up.railway.app/cert-created/${localStorage.getItem(
+          "userId"
+        )}`,
+        {
+          fullName: fullName,
+          collegeName: collegeName,
+        }
+      );
+      toast.success("Successfully Submitted", {
+        position: "top-center",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        theme: "colored",
+      });
+      window.location.reload();
+    } catch (error) {
+      toast.error("Bad network error,Please try again after sometime", {
+        position: "top-center",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        theme: "colored",
+      });
+    }
   };
 
   return (
@@ -420,9 +499,9 @@ function EndeavourProfile() {
                 ? "bg-black dark:bg-white text-white dark:text-black transition-all ease-out w-full flex rounded-[15px_0px_0px_15px]"
                 : ""
             } `}
-            onClick={paymentClicked}
+            onClick={certClicked}
           >
-            Payments
+            Certificates
           </butto>
         </div>
 
@@ -452,13 +531,11 @@ function EndeavourProfile() {
           <div className="flex flex-col justify-center items-center">
             <img
               onClick={paymentClicked}
-              src={
-                localStorage.getItem("theme") == "light" ? paymentb : paymentw
-              }
+              src={localStorage.getItem("theme") == "light" ? certb : certw}
               className="w-10 h-10"
               alt=""
             />
-            <p>Payments</p>
+            <p>Certificates</p>
           </div>
         </div>
 
@@ -839,19 +916,12 @@ function EndeavourProfile() {
               <p className="mt-11">Not Registered</p>
             )}
           </div>
-        </div>
-
-        <div
-          className={`${
-            (tab == 0 || tab == 1) && "hidden"
-          } flex w-full flex-col justify-start items-center md:py-11 px-8 pb-11 min-h-[100vh] md:p-0`}
-        >
           <div className="w-full pt-[13vh] text-center">
             <h1 className="text-4xl font-bold underline">Payments</h1>
           </div>
           <div className="w-full flex justify-center items-center">
             {paymentInfo.length !== 0 ? (
-              <div className="flex flex-wrap w-full justify-around mt-5 md:mt-11 mb-8 p-8 md:p-8">
+              <div className="flex flex-wrap w-full  gap-8 mt-5 md:mt-11 mb-8 p-8 md:p-8">
                 {paymentInfo.map((payment) => {
                   const event = eventNamePayment.find(
                     (event) => event.id === payment.eventId
@@ -859,7 +929,7 @@ function EndeavourProfile() {
                   return (
                     <div
                       key={payment._id}
-                      className="p-5 px-11 shadow-2xl rounded-lg flex flex-col bg-[#9ea4ff] justify-center items-center border-white border-4 mb-5 text-center text-[1.1em] text-black w-[80%] md:w-[30%]"
+                      className="p-5 px-11 shadow-2xl w-[80%] md:w-[30%] rounded-lg flex flex-col bg-[#9ea4ff] justify-center items-center border-white border-4 text-center text-[1.1em] text-black"
                     >
                       <p>
                         <span className="font-semibold">Payment ID :</span>
@@ -879,9 +949,7 @@ function EndeavourProfile() {
             )}
           </div>
           <div className="w-full pt-[13vh] text-center flex justify-center flex-col items-center">
-            <h1 className="text-4xl font-bold underline mb-11">
-              Entertainment Eve Payment
-            </h1>
+            <h1 className="text-4xl font-bold underline mb-11">Eve Payments</h1>
             {eveInfo ? (
               <div className="p-5 px-11 shadow-2xl w-[80%] md:w-[30%] rounded-lg flex flex-col bg-[#9ea4ff] justify-center items-center border-white border-4 text-center text-[1.1em] text-black ">
                 <p>
@@ -897,6 +965,136 @@ function EndeavourProfile() {
               <p className="mt-5">Not Registered Yet</p>
             )}
           </div>
+        </div>
+
+        <div
+          className={`${
+            (tab == 0 || tab == 1) && "hidden"
+          } flex w-full flex-col justify-start relative items-center md:py-11 px-8 pb-11 min-h-[100vh] md:p-0`}
+        >
+          <div
+            className={`${
+              getName && "hidden"
+            } bg-[#ffffffb7] dark:bg-[#000000b7] backdrop-blur-3xl absolute top-0 left-0 w-full h-[110vh] z-10 flex justify-center items-center`}
+          >
+            <div className="w-[70%] h-[50%] bg-white dark:bg-black rounded-lg flex flex-col items-center justify-center">
+              <div className="flex flex-col items-center">
+                <h1 className="text-2xl font-bold underline mb-1">
+                  Certificate
+                </h1>
+                <p className="text-red-500">
+                  *Fill it carefully you will only get one chance
+                </p>
+              </div>
+              <div className="flex flex-col w-[80%] md:w-[60%] mt-4">
+                <label
+                  className="font-semibold text-gray-600 dark:text-[#acacac]"
+                  htmlFor="fullName"
+                >
+                  Your Full Name (to be written on certificate) :
+                </label>
+                <input
+                  className="p-2 bg-transparent border-2 border-black dark:border-[#dcdcdc] rounded-lg focus:outline-none font-semibold"
+                  type="text"
+                  name="fullName"
+                  id="fullName"
+                  value={fullName}
+                  onChange={(e) => setFullName(e.target.value)}
+                />
+              </div>
+              <div className="flex flex-col w-[80%] md:w-[60%] mt-2">
+                <label
+                  className="font-semibold text-gray-600 dark:text-[#acacac]"
+                  htmlFor="collegeName"
+                >
+                  Your College Name:
+                </label>
+                <input
+                  className="p-2 bg-transparent border-2 border-black dark:border-[#dcdcdc] rounded-lg focus:outline-none font-semibold"
+                  type="text"
+                  name="collegeName"
+                  id="collegeName"
+                  value={collegeName}
+                  onChange={(e) => setCollegeName(e.target.value)}
+                />
+              </div>
+              <button
+                className="z-10 mt-5 border-2 p-2 cursor-pointer border-[#7982ff] bg-[#4d55ba] text-white rounded-md py-2 w-[50%] md:w-[30%] font-bold text-md"
+                onClick={certCreate}
+              >
+                Submit
+              </button>
+            </div>
+          </div>
+          <div className="w-full pt-[13vh] text-center">
+            <h1 className="text-4xl font-bold underline mb-8">Certificates</h1>
+          </div>
+
+          {participatedEventNames.length ? (
+            participatedEventNames.map((evnt, index) => (
+              <div key={index} className={`cert w-full h-full   flex-col `}>
+                <div
+                  className="relative"
+                  ref={
+                    participatedEventNames[index] == "B-Quiz"
+                      ? certificateWrapper1
+                      : participatedEventNames[index] == "B-Plan"
+                      ? certificateWrapper2
+                      : participatedEventNames[index] == "Market-Watch"
+                      ? certificateWrapper3
+                      : participatedEventNames[index] == "IPL Mania"
+                      ? certificateWrapper4
+                      : participatedEventNames[index] == "Movie-A-thon"
+                      ? certificateWrapper5
+                      : participatedEventNames[index] == "E-Sports(BGMI)"
+                      ? certificateWrapper6
+                      : participatedEventNames[index] == "Treasure Hunt"
+                      ? certificateWrapper7
+                      : ""
+                  }
+                >
+                  <img src={certt} alt="" className="w-full h-full" />
+                  <div className="absolute w-full top-[53%] left-[0%] text-center  text-black font-semibold text-[0.9em] md:text-4xl">
+                    <p className="text-center certFields">{fullName}</p>
+                  </div>
+                  <div className="absolute w-full top-[62.6%] left-[-4%] text-center  text-black font-semibold text-[0.5em] md:text-xl">
+                    <p className="text-center certFields">{collegeName}</p>
+                  </div>
+                  <div className="absolute w-full top-[67%] left-[-4%] text-center  text-black font-semibold text-[0.5em] md:text-xl">
+                    <p className="text-center certFields">{evnt}</p>
+                  </div>
+                </div>
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+
+                    participatedEventNames[index] == "B-Quiz"
+                      ? exportComponentAsPNG(certificateWrapper1)
+                      : participatedEventNames[index] == "B-Plan"
+                      ? exportComponentAsPNG(certificateWrapper2)
+                      : participatedEventNames[index] == "Market-Watch"
+                      ? exportComponentAsPNG(certificateWrapper3)
+                      : participatedEventNames[index] == "IPL Mania"
+                      ? exportComponentAsPNG(certificateWrapper4)
+                      : participatedEventNames[index] == "Movie-A-thon"
+                      ? exportComponentAsPNG(certificateWrapper5)
+                      : participatedEventNames[index] == "E-Sports(BGMI)"
+                      ? exportComponentAsPNG(certificateWrapper6)
+                      : participatedEventNames[index] == "Treasure Hunt"
+                      ? exportComponentAsPNG(certificateWrapper7)
+                      : "";
+
+                    // exportComponentAsPNG(certificateWrapper1);
+                  }}
+                  className="bg-[#003557] text-white font-medium mt-5 p-3 mb-5 w-full"
+                >
+                  Download
+                </button>
+              </div>
+            ))
+          ) : (
+            <p className="mt-5 z-20 text-white">No Certificates</p>
+          )}
         </div>
       </div>
     </div>
