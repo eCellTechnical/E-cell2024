a8da57d96945acd0bc631c7405e7aba011bc2e03
vaibhavgupta5 <vaibhavgupta.v890@gmail.com>
2025-04-19 22:25:00 +0530

    update

--- src/IdeateX/components/Header.jsx
+++ src/IdeateX/components/Header.jsx
@@ -102,12 +102,12 @@ function Header() {
             >
               SPONSORS
             </a>
-            <a
+            {/* <a
               href="/endeavour/eve"
               className="font-semibold text-white hover:text-[#00f699] transition-colors"
             >
              ENTERTAINMENT EVE
-            </a>
+            </a> */}
             
             <div className="flex items-center gap-3">
               {isLoggedIn ? (
@@ -198,13 +198,13 @@ function Header() {
             >
               SPONSORS
             </a>
-            <a
+            {/* <a
               href="/endeavour/eve"
               onClick={toggleMenu}
               className="text-white font-semibold text-xl hover:text-[#00f699]"
             >
               ENTERTAINMENT EVE
-            </a>
+            </a> */}
           </nav>
           <div className="mt-8 flex flex-col sm:flex-row gap-4">
             {isLoggedIn ? (
